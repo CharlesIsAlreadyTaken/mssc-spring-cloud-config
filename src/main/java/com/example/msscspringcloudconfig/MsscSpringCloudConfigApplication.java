9683ad9fec2d543302ab616dd880d7ea57a8f8f4
CharlesIsAlreadyTaken <charlesalbaut@gmail.com>
2021-06-09 06:09:15 -0400

    Spring init

---
+++ src/main/java/com/example/msscspringcloudconfig/MsscSpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.msscspringcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscSpringCloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscSpringCloudConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msscspringcloudconfig/MsscSpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msscspringcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
